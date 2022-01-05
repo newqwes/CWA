9172f18ee101896bddc977844fd73340a79c2fdd
Nazar Ashyrov <newnewqwes@gmail.com>
2022-01-05 11:49:50 +0300

    Release/v0.1.8 (#28)
    * fixed a bug when the user could not get data from the database
    
    update github workflows
    
    * fix: refactor mail form
    
    * front-feature: Configured new routers for pagesand some refactoring
    
    * Add ag-grid and apexcharts libs try some design on statistic tab
    
    * bugfix: close window
    
    * added two endpoints for creating and reading orders, refactoring
    
    * Google authorization button, display if not logged in, add button, table refactor
    
    * Nesting in a table, a form for adding a transaction, refactoring of statistics components
    
    * receiving and processing all data
    
    * added the ability to count data
    
    * Release/v0.1.6 (#26)
    
    * Release/v0.1.2 (#19)
    
    * fixed a bug when the user could not get data from the database
    
    update github workflows
    
    * fix: refactor mail form
    
    * front-feature: Configured new routers for pagesand some refactoring
    
    * Release/v0.1.3 (#20)
    
    * fixed a bug when the user could not get data from the database
    
    update github workflows
    
    * fix: refactor mail form
    
    * front-feature: Configured new routers for pagesand some refactoring
    
    * Add ag-grid and apexcharts libs try some design on statistic tab
    
    * hotfix: Do not close window (#21)
    
    * fixed a bug when the user could not get data from the database
    
    update github workflows
    
    * fix: refactor mail form
    
    * front-feature: Configured new routers for pagesand some refactoring
    
    * Add ag-grid and apexcharts libs try some design on statistic tab
    
    * bugfix: close window
    
    * Release/v0.1.4 (#22)
    
    * fixed a bug when the user could not get data from the database
    
    update github workflows
    
    * fix: refactor mail form
    
    * front-feature: Configured new routers for pagesand some refactoring
    
    * Add ag-grid and apexcharts libs try some design on statistic tab
    
    * bugfix: close window
    
    * added two endpoints for creating and reading orders, refactoring
    
    * Release/v0.1.5 (#23)
    
    * fixed a bug when the user could not get data from the database
    
    update github workflows
    
    * fix: refactor mail form
    
    * front-feature: Configured new routers for pagesand some refactoring
    
    * Add ag-grid and apexcharts libs try some design on statistic tab
    
    * bugfix: close window
    
    * added two endpoints for creating and reading orders, refactoring
    
    * Google authorization button, display if not logged in, add button, table refactor
    
    * Nesting in a table, a form for adding a transaction, refactoring of statistics components
    
    * Release/v0.1.6 (#24)
    
    * fixed a bug when the user could not get data from the database
    
    update github workflows
    
    * fix: refactor mail form
    
    * front-feature: Configured new routers for pagesand some refactoring
    
    * Add ag-grid and apexcharts libs try some design on statistic tab
    
    * bugfix: close window
    
    * added two endpoints for creating and reading orders, refactoring
    
    * Google authorization button, display if not logged in, add button, table refactor
    
    * Nesting in a table, a form for adding a transaction, refactoring of statistics components
    
    * receiving and processing all data
    
    * added the ability to count data
    
    * hotfix
    
    * added the ability to count data
    
    * fix token limit

--- back-end/src/utils/token.js
+++ back-end/src/utils/token.js
@@ -3,12 +3,17 @@ import jwt from 'jsonwebtoken';
 /**
  * @param {Object} payload
  * @param {Object} payload.id userId
- * @param {Object} payload.login
+ * @param {Object} payload.email
  * @returns {Object}
  */
-export const generateTokens = payload => {
-  const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '60m' });
-  const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '30d' });
+export const generateTokens = ({ email, id }) => {
+  const accessToken = jwt.sign({ email, id }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '60m',
+  });
+  const refreshToken = jwt.sign({ email, id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: '30d',
+  });
+
   return {
     accessToken,
     refreshToken,
