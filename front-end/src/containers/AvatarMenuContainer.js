e02dd2547ddee9376a883371c7502fbde26fb0bb
Nazar Ashyrov <newnewqwes@gmail.com>
2022-06-19 17:50:02 +0300

    Refactor user menu component (#76)

--- front-end/src/containers/AvatarMenuContainer.js
+++ front-end/src/containers/AvatarMenuContainer.js
@@ -2,23 +2,20 @@ import { connect } from 'react-redux';
 
 import { deleteUserAC } from '../actionCreators/user';
 import { authLogoutAC } from '../actionCreators/auth';
+import { userMenuConfig } from '../configuration/userMenuConfig';
 
 import AvatarMenu from '../components/AvatarMenu';
-
-import { deleteUserModalConfig } from '../configuration/deleteUserConfig';
-import { logoutModalConfig } from '../configuration/authConfig';
 
 const mapDispatchToProps = {
   deleteUser: deleteUserAC,
   authLogout: authLogoutAC,
 };
 
-const mergeProps = (_, { authLogout, deleteUser }, ownProps) => ({
-  ...ownProps,
-  authLogout,
-  deleteUser,
-  deleteUserModalConfig,
-  logoutModalConfig,
+const mergeProps = (_, { deleteUser, authLogout }) => ({
+  menuItems: [
+    { ...userMenuConfig.delete, onClick: userMenuConfig.delete.onClickFactory(deleteUser) },
+    { ...userMenuConfig.logout, onClick: userMenuConfig.logout.onClickFactory(authLogout) },
+  ],
 });
 
 export default connect(null, mapDispatchToProps, mergeProps)(AvatarMenu);
