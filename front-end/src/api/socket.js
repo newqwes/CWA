b0f16bbc7e08672ac0a2147afca0b0ee22e1341a
newqwes <newnewqwes@gmail.com>
2023-05-09 19:03:20 +0000

    fix

--- front-end/src/api/socket.js
+++ front-end/src/api/socket.js
@@ -1,6 +1,5 @@
 import io from 'socket.io-client';
 
-// const socket = io('localhost:3015');
-const socket = io('https://coinlitics.space');
+const socket = io(process.env.SERVER_URL);
 
 export default socket;
