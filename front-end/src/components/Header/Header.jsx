4cf04a88b3b761ebd4cadfe1f8763de462f09f4b
newqwes <newnewqwes@gmail.com>
2023-05-09 19:57:38 +0000

    fix timer

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { floor } from 'lodash/fp';
 
@@ -16,113 +16,104 @@ import { AUTH_TELEGRAM_CODE_PREFIX } from '../../constants/telegram';
 
 const UPDATE_TIMER_MILLISECONDS = 1000;
 
-class Header extends React.Component {
-  static propTypes = {
-    handleRefresh: PropTypes.func.isRequired,
-    lastDateUpdate: PropTypes.string,
-    score: PropTypes.number,
-    dataRefreshLimitPerMinute: PropTypes.number,
-    children: PropTypes.node.isRequired,
-    authorized: PropTypes.bool.isRequired,
-    userId: PropTypes.string,
-    noData: PropTypes.bool,
-    bank: PropTypes.number.isRequired,
-  };
+const Header = ({
+                  handleRefresh,
+                  lastDateUpdate,
+                  score,
+                  dataRefreshLimitPerMinute,
+                  children,
+                  authorized,
+                  userId,
+                  noData,
+                  bank,
+                }) => {
+  const [refreshTimer, setRefreshTimer] = useState(0);
+  const [refreshDisabled, setRefreshDisabled] = useState(false);
 
-  state = {
-    refreshTimer: 0,
-    refreshDisabled: false,
-  };
+  useEffect(() => {
+    const refreshTimerValue = getRefreshTimer(lastDateUpdate, dataRefreshLimitPerMinute);
 
-  componentDidUpdate(prevProps) {
-    const { lastDateUpdate, dataRefreshLimitPerMinute } = this.props;
-
-    if (lastDateUpdate !== prevProps.lastDateUpdate) {
-      const refreshTimer = getRefreshTimer(lastDateUpdate, dataRefreshLimitPerMinute);
-
-      if (refreshTimer <= 0) {
-        return;
-      }
-
-      this.setState({ refreshTimer, refreshDisabled: true });
-
-      this.timer = setTimeout(this.tick, UPDATE_TIMER_MILLISECONDS);
+    if (refreshTimerValue <= 0) {
+      return;
     }
-  }
 
-  componentWillUnmount() {
-    this.transition();
-  }
+    setRefreshTimer(refreshTimerValue);
+    setRefreshDisabled(true);
 
-  tick = () => {
-    const { refreshTimer } = this.state;
+    const timer = setInterval(() => {
+      setRefreshTimer((prevTimer) => {
+        if (prevTimer <= 1) {
+          setRefreshDisabled(false);
+          clearInterval(timer);
+          return 0;
+        }
 
-    this.transition();
-
-    if (refreshTimer <= 1) {
-      this.setState({ refreshTimer: 0, refreshDisabled: false });
-    } else {
-      this.setState({
-        refreshTimer: refreshTimer - 1,
-        refreshDisabled: true,
+        return prevTimer - 1;
       });
+    }, UPDATE_TIMER_MILLISECONDS);
 
-      this.timer = setTimeout(this.tick, UPDATE_TIMER_MILLISECONDS);
-    }
-  };
+    return () => {
+      clearInterval(timer);
+    };
+  }, [lastDateUpdate, dataRefreshLimitPerMinute]);
 
-  transition() {
-    clearInterval(this.timer);
-  }
+  useEffect(() => {
+    const refreshTimerValue = getRefreshTimer(lastDateUpdate, dataRefreshLimitPerMinute);
+    setRefreshTimer(refreshTimerValue);
+    setRefreshDisabled(refreshTimerValue > 0);
+  }, [lastDateUpdate, dataRefreshLimitPerMinute]);
 
-  render() {
-    const { score, handleRefresh, children, authorized, userId, noData, bank } = this.props;
-    const { refreshTimer, refreshDisabled } = this.state;
+  const time = refreshTimer > 100 ? `${floor(refreshTimer / 60)} мин.` : `${floor(refreshTimer)} сек.`;
 
-    const time =
-      refreshTimer > 100 ? `${floor(refreshTimer / 60)} мин.` : `${floor(refreshTimer)} сек.`;
+  return (
+    <HeaderWrapper>
+      <Paragraph>Банк: {bank} cwa</Paragraph>
+      <Paragraph
+        copyable={{
+          text: AUTH_TELEGRAM_CODE_PREFIX + userId,
+          tooltips: ['Скопировать Ваш личный ключ для телеграмма', 'Ключ скопирован!'],
+        }}
+      >
+        Ключ для авторизации в телеграм
+      </Paragraph>
+      <AuthBlock>
+        {authorized && (
+          <>
+            <Tooltip title="Количество Вашей игровой валюты">
+              <Title level={4}>{score} cwa</Title>
+            </Tooltip>
+            {refreshDisabled ? (
+              <Text>Осталось: {time}</Text>
+            ) : (
+              <div>
+                <Button
+                  onClick={handleRefresh}
+                  type="primary"
+                  loading={refreshDisabled}
+                  disabled={refreshDisabled || noData}
+                >
+                  Обновить
+                </Button>
+              </div>
+            )}
+          </>
+        )}
+        {children}
+      </AuthBlock>
+    </HeaderWrapper>
+  );
+};
 
-    return (
-      <HeaderWrapper>
-        <Paragraph>Банк: {bank} cwa</Paragraph>
-        <Paragraph
-          copyable={{
-            text: AUTH_TELEGRAM_CODE_PREFIX + userId,
-            tooltips: [
-              'Скопировать Ваш личный ключ для телеграмма',
-              'Ключ скопирован!',
-            ],
-          }}
-        >
-          Ключ для авторизации в телеграм
-        </Paragraph>
-        <AuthBlock>
-          {authorized && (
-            <>
-              <Tooltip title="Количество Вашей игровой валюты">
-                <Title level={4}>{score} cwa</Title>
-              </Tooltip>
-              {refreshDisabled ? (
-                <Text>Осталось: {time}</Text>
-              ) : (
-                <div>
-                  <Button
-                    onClick={handleRefresh}
-                    type="primary"
-                    loading={refreshDisabled}
-                    disabled={refreshDisabled || noData}
-                  >
-                    Обновить
-                  </Button>
-                </div>
-              )}
-            </>
-          )}
-          {children}
-        </AuthBlock>
-      </HeaderWrapper>
-    );
-  }
-}
+Header.propTypes = {
+  handleRefresh: PropTypes.func.isRequired,
+  lastDateUpdate: PropTypes.string,
+  score: PropTypes.number,
+  dataRefreshLimitPerMinute: PropTypes.number,
+  children: PropTypes.node.isRequired,
+  authorized: PropTypes.bool.isRequired,
+  userId: PropTypes.string,
+  noData: PropTypes.bool,
+  bank: PropTypes.number.isRequired,
+};
 
 export default Header;
