9f3b3e559135f5ddb74c4b023f551d87c6e7fc31
newqwes <newnewqwes@gmail.com>
2023-07-14 18:36:06 +0300

    fix timer

--- front-end/src/utils/refresh.js
+++ front-end/src/utils/refresh.js
@@ -1,4 +1,10 @@
 const minuteToMillisecond = minute => minute * 60 * 1000;
 
-export const getRefreshTimer = (lastDateUpdate, dataRefreshLimitPerMinute) =>
-  (Date.parse(lastDateUpdate) + minuteToMillisecond(dataRefreshLimitPerMinute) - Date.now()) / 1000;
+export const getRefreshTimer = (lastDateUpdate, dataRefreshLimitPerMinute) => {
+  if (Date.parse(lastDateUpdate) < Date.now() - minuteToMillisecond(2)) {
+    return 0;
+  }
+  return (
+    Date.parse(lastDateUpdate) + minuteToMillisecond(dataRefreshLimitPerMinute) - Date.now()
+  ) / 1000;
+};
