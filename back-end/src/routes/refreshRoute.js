06aca387a25a9478bd01eaded4fde15f50383b0b
newqwes <newnewqwes@gmail.com>
2024-03-16 11:05:55 +0300

    Add logger

--- back-end/src/routes/refreshRoute.js
+++ back-end/src/routes/refreshRoute.js
@@ -1,13 +1,13 @@
 import express from 'express';
 
 // import checkLastRefreshDate from '../middleware/checkLastRefreshDate';
-import authMiddleware from '../middleware/authMiddleware';
 import { refresh } from '../controllers/refreshController';
 
 const refreshRoute = express.Router();
 
 // NOTE: Add Middleware to check whether the user is active or not, remove checkLastRefreshDate
 // refreshRoute.post('/', authMiddleware, checkLastRefreshDate, refresh);
-refreshRoute.post('/', authMiddleware, refresh);
+// refreshRoute.post('/', authMiddleware, refresh);
+refreshRoute.post('/', refresh);
 
 export default refreshRoute;
